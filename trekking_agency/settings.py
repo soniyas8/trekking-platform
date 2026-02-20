c25586826186a78df659df85baa1e6ce78ece884
soniyas8 <157476457+soniyas8@users.noreply.github.com>
2026-02-20 15:25:54 -0500

    Fix media file storage configuration

--- trekking_agency/settings.py
+++ trekking_agency/settings.py
@@ -130,6 +130,9 @@ MEDIA_ROOT = BASE_DIR / 'media'
 
 # WhiteNoise configuration
 STORAGES = {
+    "default": {
+        "BACKEND": "django.core.files.storage.FileSystemStorage",
+    },
     "staticfiles": {
         "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
     },
